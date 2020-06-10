773b8c290147dcd90f401644d386bf5b23db773f
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-06-10 12:02:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import NavBar from "./component/NavBar"
 import Footer from "./component/Footer"
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {Container, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
+
+
 
 
 
@@ -11,6 +14,30 @@ function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Container fluid className="px-4">
+      <Row className = "justify-content-between">
+        <Col className = "d-flex">
+        <h2 className = "mb-4">TV Shows</h2>
+        <Dropdown>
+          <Dropdown.Toggle
+            variant="secondary"
+            id="drowpdown-basic"
+            className="btn btn sm dropdown-toggle rounded-0 ml-3"
+            style={{backgroundColor: "#221f1f" }}
+            
+          >
+          Genre
+          </Dropdown.Toggle>
+        <Dropdown.Menu className ="bg-dark">
+        <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
+       </Dropdown.Menu>
+       </Dropdown>
+
+        </Col>
+      </Row>
+     </Container>
      <Footer/>
     </div>
   );
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 const Footer = () => (
     <footer>
     <div className="row text-center mt-5">
